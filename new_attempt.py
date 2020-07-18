a75efce79ab170e0c81fa2c1581e4d452314f197
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-18 16:17:19 +0300

    Пытался чеерз словарь - фиг вышло. Осталось убрать дубли строк

--- new_attempt.py
+++ new_attempt.py
@@ -13,16 +13,17 @@ with open("phonebook_raw.csv") as f:
     contacts_list = list(rows)
 new_contact_list = [] #'lastname', 'firstname', 'surname', 'organization', 'position', 'phone', 'email']
 lastnamelist = []
-for cell in contacts_list:
-    surname = cell[2].split()
-    firstname = cell[1].split()
-    lastname = cell[0].split()
-    organization = cell[3]
-    position = cell[4]
-    phone = cell[5]
+contact_dict = {}
+for string in contacts_list:
+    surname = string[2].split()
+    firstname = string[1].split()
+    lastname = string[0].split()
+    organization = string[3]
+    position = string[4]
+    phone = string[5]
     phone = phone_pattern.sub(r'+7(\2)\3-\4-\5', phone)
     phone = ext_phone_pattern.sub(r'доб.\1', phone)
-    email = cell[6]
+    email = string[6]
 
     if len(lastname) == 3:
         surname = lastname[2]
@@ -44,18 +45,41 @@ for cell in contacts_list:
 
 
     new_contact_string = [lastname, firstname, surname, organization, position, phone, email]
-    lastnamelist.append(lastname)
     new_contact_list.append(new_contact_string)
-# print(set(lastnamelist))
-# pprint(new_contact_list)
-upd_contact_string = []
-for contact_string in new_contact_list:
-    if contact_string[0] in set(lastnamelist):
-        for item in contact_string:
-            if item:
-                upd_contact_string.append(item)
-        # upd_contact_string = [contact_string[0], contact_string[1], contact_string[2], contact_string[3]]
-pprint(upd_contact_string)
+
+'''
+Попытка запилить через словарь
+
+    string_dict = {
+        lastname:
+            {
+            'firstname': firstname,
+            'surname': surname,
+            'organization': organization,
+            'position': position,
+            'phone': phone,
+            'email': email}
+    }
+    # print(string_dict)
+    for key, value in string_dict.items():
+        if key in contact_dict.keys():
+            if value:
+                string_dict[key].update(value)
+                print(key, value)
+        else:
+            contact_dict.update(string_dict)
+pprint(contact_dict)
+'''
+
+pprint(new_contact_list)
+
+''' Пытаюсь понять как проходя по списку проверять что такое значение уже было получено ранее и 
+если поле непустое то заполнить его.
+'''
+# for item in new_contact_list:
+#     for piese in item:
+#         if piese:
+#             print(piese)
 # with open("phonebook.csv", "w") as f:
 #     datawriter = csv.writer(f, delimiter=',')
 #     datawriter.writerows(new_contact_list)
